f89c2eff2bb231f9aff55bd834eeaccc5558e1dd
Marek Rychlik <rychlik@arizona.edu>
2023-03-30 11:21:48 -0700

    *** empty log message ***

--- src/main/java/com/marekrychlik/Demo/CLITester.java
+++ src/main/java/com/marekrychlik/Demo/CLITester.java
@@ -13,89 +13,89 @@ import java.io.PrintWriter;
 
 
 public class CLITester {
-   public static void main(String[] args) throws ParseException {
-      //***Definition Stage***
-      // create Options object
-      Options options = new Options();
+    public static void main(String[] args) throws ParseException {
+	//***Definition Stage***
+	// create Options object
+	Options options = new Options();
       
-      // add option "-h"
-      options.addOption("h",false,"print help");
+	// add option "-h"
+	options.addOption("h",false,"print help");
 
 
-      // add option "-a"
-      options.addOption("a", false, "add numbers");
+	// add option "-a"
+	options.addOption("a", false, "add numbers");
       
-      // add option "-m"
-      options.addOption("m", false, "multiply numbers");
+	// add option "-m"
+	options.addOption("m", false, "multiply numbers");
 
-      Option logfile = Option.builder()
-          .longOpt("logFile")
-          .argName("file" )
-          .hasArg()
-          .desc("use given file for log" )
-          .build();
+	Option logfile = Option.builder()
+	    .longOpt("logFile")
+	    .argName("file" )
+	    .hasArg()
+	    .desc("use given file for log" )
+	    .build();
 
-      options.addOption(logfile);
+	options.addOption(logfile);
 
-      Option propertyOption = Option.builder()
-	  .longOpt("D")
-	  .argName("property=value" )
-	  .hasArg()
-	  .valueSeparator()
-	  .numberOfArgs(2)
-	  .desc("use value for given properties" )
-	  .build();
+	Option propertyOption = Option.builder()
+	    .longOpt("D")
+	    .argName("property=value" )
+	    .hasArg()
+	    .valueSeparator()
+	    .numberOfArgs(2)
+	    .desc("use value for given properties" )
+	    .build();
 
-      options.addOption(propertyOption);
+	options.addOption(propertyOption);
       
-      //***Parsing Stage***
-      //Create a parser
-      CommandLineParser parser = new DefaultParser();
+	//***Parsing Stage***
+	//Create a parser
+	CommandLineParser parser = new DefaultParser();
 
-      //parse the options passed as command line arguments
-      CommandLine cmd = parser.parse( options, args);
+	//parse the options passed as command line arguments
+	CommandLine cmd = parser.parse( options, args);
 
-      //***Interrogation Stage***
-      //hasOptions checks if option is present or not
-      if(cmd.hasOption("a")) {
-         System.out.println("Sum of the numbers: " + getSum(args));
-      } else if(cmd.hasOption("m")) {
-         System.out.println("Multiplication of the numbers: " + getMultiplication(args));
-      }
+	//***Interrogation Stage***
+	//hasOptions checks if option is present or not
+	if(cmd.hasOption("a")) {
+	    System.out.println("Sum of the numbers: " + getSum(args));
+	} else if(cmd.hasOption("m")) {
+	    System.out.println("Multiplication of the numbers: " + getMultiplication(args));
+	}
 
-      // has the logFile argument been passed?
-      if(cmd.hasOption("logFile")) {
-         //get the logFile argument passed
-         System.out.println( cmd.getOptionValue( "logFile" ) );
-      }
+	// has the logFile argument been passed?
+	if(cmd.hasOption("logFile")) {
+	    //get the logFile argument passed
+	    System.out.println( cmd.getOptionValue( "logFile" ) );
+	}
 
-      if(cmd.hasOption("D")) {
-         Properties properties = cmd.getOptionProperties("D");
-         System.out.println("Log: " + properties.getProperty("logFile"));
-         System.out.println("Operator: " + properties.getProperty("operator"));
-      }
+	if(cmd.hasOption("D")) {
+	    Properties properties = cmd.getOptionProperties("D");
+	    System.out.println("Log: " + properties.getProperty("logFile"));
+	    System.out.println("Operator: " + properties.getProperty("operator"));
+	}
 
-      if(cmd.hasOption("h")) {
-	  usage(options);
-      }
+	if(cmd.hasOption("h")) {
+	    usage(options);
+	}
 	  
 	 
 
-   }
-   public static int getSum(String[] args) {
-      int sum = 0;
-      for(int i = 1; i < args.length ; i++) {
-         sum += Integer.parseInt(args[i]);
-      }
-      return sum;
-   }
-   public static int getMultiplication(String[] args) {
-      int multiplication = 1;
-      for(int i = 1; i < args.length ; i++) {
-         multiplication *= Integer.parseInt(args[i]);
-      }
-      return multiplication;
-   }
+    }
+    public static int getSum(String[] args) {
+	int sum = 0;
+	for(int i = 1; i < args.length ; i++) {
+	    sum += Integer.parseInt(args[i]);
+	}
+	return sum;
+    }
+    public static int getMultiplication(String[] args) {
+	int multiplication = 1;
+	for(int i = 1; i < args.length ; i++) {
+	    multiplication *= Integer.parseInt(args[i]);
+	}
+	return multiplication;
+    }
 
     private static void usage(Options options) {
 	HelpFormatter formatter = new HelpFormatter();
